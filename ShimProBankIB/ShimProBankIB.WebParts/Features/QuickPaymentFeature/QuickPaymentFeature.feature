4106096e2bba6c5f3da8f9ebab1502fffac0f645
roman.prokhodsky <roman.prokhodsky@4f59a101-5c4f-52d0-1c35-3c01d6f8d369>
2012-10-03 09:29:42 +0000

    Added webparts and common projects

---
+++ ShimProBankIB/ShimProBankIB.WebParts/Features/QuickPaymentFeature/QuickPaymentFeature.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="e340e33d-375f-4c50-85dd-d33420ecbffc" description="Activates quick payment webpart." featureId="e340e33d-375f-4c50-85dd-d33420ecbffc" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Quick Payment webpart feature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="cf56b883-f536-4928-a152-6872c0189927" />
+  </projectItems>
+</feature>
---
+++ ShimProBankIB/ShimProBankIB.WebParts/QuickPaymentWebPart/QuickPaymentWebPart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace ShimProBankIB.WebParts.QuickPaymentWebPart
+{
+	[ToolboxItemAttribute(false)]
+	public class QuickPaymentWebPart : WebPart
+	{
+		// Visual Studio might automatically update this path when you change the Visual Web Part project item.
+		private const string _ascxPath = @"~/_CONTROLTEMPLATES/ShimProBankIB.WebParts/QuickPaymentWebPart/QuickPaymentWebPartUserControl.ascx";
+
+		protected override void CreateChildControls()
+		{
+			Control control = Page.LoadControl(_ascxPath);
+			Controls.Add(control);
+		}
+	}
+}
